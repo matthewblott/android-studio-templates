492b2ceb8a44f7b24709335644c96e0e02b27ad0
Matthew Blott <m4ttb@pm.me>
2024-06-28 17:02:42 +0100

    saved

---
+++ pico-basic-views-activity/app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+  id("com.android.application") version "8.4.2"
+  id("org.jetbrains.kotlin.android") version "1.9.0"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+}
+
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+
+}
+
+android {
+  namespace = "com.example.app"
+  compileSdk = 34
+
+  defaultConfig {
+    minSdk = 34
+  }
+
+  buildFeatures {
+    viewBinding = true
+  }
+
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_18.toString()
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_18
+    targetCompatibility = JavaVersion.VERSION_18
+  }
+
+}
+
+dependencies {
+  implementation("androidx.appcompat:appcompat:1.7.0")
+  implementation("androidx.browser:browser:1.8.0")
+  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+  implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+}
+
+repositories {
+  google()
+  mavenCentral()
+}
---
+++ pico-basic-views-activity/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var appBarConfiguration: AppBarConfiguration
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+    setContentView(binding.root)
+
+    setSupportActionBar(binding.toolbar)
+
+    val navController = findNavController(R.id.content_main)
+    appBarConfiguration = AppBarConfiguration(navController.graph)
+    setupActionBarWithNavController(navController, appBarConfiguration)
+  }
+
+  // Required for the back / up button
+  override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.content_main)
+    return navController.navigateUp(appBarConfiguration)
+        || super.onSupportNavigateUp()
+  }
+}
